b5cb335c83e6bc8ad143f9c9545f2e6efc2f66e3
craig <mrman2289@gmail.com>
2017-10-07 23:23:12 +0100

    starting to introduce babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import { render } from 'react-dom';
+
 
 import bulma from 'bulma/css/bulma.css';
+
+// const Hello = () => (<h1>Hello!</h1>);
+
+// render(<Hello/>, document.body);
+
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
                         {
                             loader: "css-loader",
                             options: {
-                                minimize: true
+                                // minimize: true
                             }
                         }
                     ]
