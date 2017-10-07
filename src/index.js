7543ee6e4a9dff08186669efdfef689afaf36eb2
craig <mrman2289@gmail.com>
2017-10-07 22:51:33 +0100

    HTML plugin to generate index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import React from 'react';
+
+import bulma from 'bulma/css/bulma.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'SquareUp'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 };
